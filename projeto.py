a379e5f72509e5fd06fb96a0b1b05a8087db1711
eunicejamorim <57901524+eunicejamorim@users.noreply.github.com>
2019-12-19 17:21:46 +0000

    Add files via upload

--- projeto.py
+++ projeto.py
@@ -76,8 +76,9 @@ MARGIN_TOP = 0
 FALL_TIME = 1000  # ms
 
 last_fall_time = 0
-pos_row = 0
-pos_col = 0
+player_row = 0
+player_col = 0
+player_color = random.randrange(1, 4+1)
 
 clock = pygame.time.Clock()
   
@@ -89,28 +90,44 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            pygame.display.quit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT and pos_col>=0:
-                if pos_col > 0:
-                    pos_col -= 1
-            if event.key == pygame.K_RIGHT and pos_col<=6 :
-                if pos_col < len(matrix[0]):
-                    pos_col += 1
+            if event.key == pygame.K_LEFT and player_col>=0:
+                if player_col > 0:
+                    player_col -= 1
+            if event.key == pygame.K_RIGHT and player_col<=6 :
+                if player_col < len(matrix[0]):
+                    player_col += 1
             if event.key == pygame.K_DOWN:
                 down_pressed = True
 
     # movimento
     if (pygame.time.get_ticks() - last_fall_time) > FALL_TIME or down_pressed:
-        if matrix[pos_row+1][pos_col] == 0:
-            pos_row += 1
+        if matrix[player_row+1][player_col] == 0:
+            player_row += 1
         else:  # colisao
-            matrix[pos_row][pos_col] = 1
-            pos_row = 0
-            pos_col = 0    
+            matrix[player_row][player_col] = player_color
+            player_row = 0
+            player_col = 0
+            player_color = random.randrange(1, 4+1)
         last_fall_time = pygame.time.get_ticks()
 
-        
+    # encontrar linhas/colunas 4 iguais
+    for col in range(8-3):
+        for row in range(14):
+            if matrix[row][col] == matrix[row][col+1] == matrix[row][col+2] == matrix[row][col+3] != 0:
+                matrix[row][col] = 0
+                matrix[row][col+1] = 0
+                matrix[row][col+2] = 0
+                matrix[row][col+3] = 0
+    for col in range(8):
+        for row in range(14-3):
+            if matrix[row][col] == matrix[row+1][col] == matrix[row+2][col] == matrix[row+3][col] != 0:
+                matrix[row][col] = 0 
+                matrix[row+1][col] = 0
+                matrix[row+2][col] = 0
+                matrix[row+3][col] = 0
+
+    ####################################################################
     # desenhar
     screen.fill((0, 0, 0))
 
@@ -123,24 +140,10 @@ while running:
             y = row*TILE_WIDTH+MARGIN_TOP
             pygame.draw.rect(screen, color, (x+2, y+2, TILE_WIDTH-4, TILE_WIDTH-4))
             #screen.blit(teste, (50, 50))  # desenhar imagem
-    x = pos_col*TILE_WIDTH+MARGIN_LEFT
-    y = pos_row*TILE_WIDTH+MARGIN_TOP
-    pygame.draw.circle(screen, (0, 255, 0), (x+TILE_WIDTH//2, y+TILE_WIDTH//2), TILE_WIDTH//2)
+    x = player_col*TILE_WIDTH+MARGIN_LEFT
+    y = player_row*TILE_WIDTH+MARGIN_TOP
+    pygame.draw.circle(screen, COLORS[player_color], (x+TILE_WIDTH//2, y+TILE_WIDTH//2), TILE_WIDTH//2)
 
     pygame.display.flip()
 
-    for col in range(8-4):
-        for row in range(14):
-            if matrix[row][col] == 1 and matrix[row][col+1] == 1 and matrix[row][col+2] == 1 and matrix[row][col+3] == 1:
-                matrix[row][col] =0 
-                matrix[row][col+1] = 0
-                matrix[row][col+2] =0
-                matrix[row][col+3] =0
-    for col in range(8):
-        for row in range(14-4):
-            if matrix[row][col] == 1 and matrix[row+1][col] == 1 and matrix[row+2][col] == 1 and matrix[row+3][col] == 1:
-                matrix[row][col] =0 
-                matrix[row+1][col] = 0
-                matrix[row+2][col] =0
-                matrix[row+3][col] =0
 pygame.quit()
